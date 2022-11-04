4df98eff8a47075e98f0d2c71f878504628f6c90
Joshua Hill <jhill42015@gmail.com>
2022-11-04 06:14:12 -0500

    Homepage Display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-
-
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-    <CampsitesDirectoryPage/>
-    <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
